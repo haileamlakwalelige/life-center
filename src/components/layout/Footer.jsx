042562349972240bdc9e51869b1c69d52f4b1081
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-02 07:15:11 +0300

    finalize footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,78 +1,104 @@
-// import React from 'react'
-import footer from '../../assets/images/footer.png';
-import logo from '../../assets/images/logo.png';
-import go from '../../assets/images/go.png';
-import tel from '../../assets/images/tel.png';
-import lin from '../../assets/images/lin.png';
-import twi from '../../assets/images/twi.png';
+import React from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+import { FaYoutube, FaLinkedin, FaTelegram, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
-  const footerStyle = {
-    backgroundImage: `url(${footer})`,
-    backgroundSize: 'cover', // Adjust this based on your design preferences
-    backgroundRepeat: 'no-repeat',
-  };
   return (
-    <footer style={footerStyle} className=" py-8 md:py-16 lg:py-24 ">
-      <div className="flex flex-col md:flex-row justify-center items-center px-2 sm:px-4 md:px-6 lg:px-8 gap-5">
-        <div className="md:w-1/2 lg:w-2/4">
+    <div className="min-h-[60vh] bg-white">
+      <div className="min-h-[60vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center items-start py-10 lg:py-20 px-2 sm:px-6 md:px-12 lg:px-20">
+        {/* Section one */}
+        <div className="flex flex-col justify-center items-center">
           <img
             src={logo}
-            alt="Habesha Digital Marketing"
-            className="w-[491px] h-[308px]"
+            alt="Life Center Ethiopian Residents Charity Organization Logo"
           />
+          <p className="text-primary text-xl text-center font-semibold">
+            Life Center Ethiopian Residents Charity Organization
+          </p>
+          <p className="mt-5 text-center">
+            Life Center Ethiopian Residents Charity Organization: Lighting hope
+            for orphans, empowering widows, and transforming lives, contributing
+            to poverty reduction in Ethiopia.
+          </p>
         </div>
-        <div className="md:w-1/2 lg:w-2/4">
-          <form>
-            <p className="font-semibold cri text-[30px] text-center text-white py-10">
-              Subscribe to Our Newsletter!
-            </p>
-            <div className="py-2">
-              <label className="text-[16px] cri text-white font-semibold pb-1">
-                Name
-              </label>
-              <input
-                type="text"
-                className="w-full rounded-2xl bg-[#226E9F] py-1 focus:outline-none border-none text-white px-3"
-              />
-            </div>
-            <div className="py-2">
-              <label className="text-[16px] cri text-white font-semibold pb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                className="w-full rounded-2xl bg-[#226E9F] py-1 focus:outline-none border-none text-white px-3"
-              />
-            </div>
-          </form>
-        </div>
-        <div className="md:w-1/2 lg:w-2/4">
-          <p className="cri font-semibold text-[30px] lg:text-[40px] text-white text-center py-3">
-            Our services
-          </p>
-          <p className="text-[18px] text-white cri text-center py-2 font-bold">
-            Website Design and Development
-          </p>
-          <p className="text-[18px] text-white cri text-center pb-2 font-bold">
-            Social Media Advertising
-          </p>
-          <p className="text-[18px] text-white cri text-center pb-2 font-bold">
-            Digital Strategy Consulting
-          </p>
-          <p className="text-[18px] text-white cri text-center pb-2 font-bold">
-            Content Marketing
-          </p>
 
-          <div className="flex justify-center items-center gap-3 md:gap-5 pt-10 lg:gap-10">
-            <img src={go} alt="Google" className="h-[40px] w-[40px]" />
-            <img src={tel} alt="Telegram" className="h-[40px] w-[40px]" />
-            <img src={lin} alt="LinkedIn" className="h-[40px] w-[40px]" />
-            <img src={twi} alt="Twitter" className="h-[40px] w-[40px]" />
+        {/* Section Two */}
+        <div className="flex flex-col justify-center items-center md:mt-20">
+          <p className="font-bold text-2xl md:text-3xl text-primary">
+            Quick Link
+          </p>
+          <div className="flex flex-col justify-center items-center gap-4 mt-4">
+            <Link
+              to="/"
+              className="text-lg font-medium text-gray-600 hover:text-secondary"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="text-lg font-medium text-gray-600 hover:text-secondary"
+            >
+              About
+            </Link>
+            <Link
+              to="/program"
+              className="text-lg font-medium text-gray-600 hover:text-secondary"
+            >
+              Program
+            </Link>
+            <Link
+              to="/blog"
+              className="text-lg font-medium text-gray-600 hover:text-secondary"
+            >
+              Blog
+            </Link>
+            <Link
+              to="/contact"
+              className="text-lg font-medium text-gray-600 hover:text-secondary"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+
+        {/* Section 3 */}
+        <div className="flex flex-col justify-center items-center md:mt-20">
+          <p className="font-bold text-2xl md:text-3xl text-primary">
+            Get In Touch
+          </p>
+          <div className="flex flex-col justify-center pl-5 lg:pl-10 items-start gap-2 mt-4">
+            <p className="text-base font-medium text-gray-600 ">
+              Phone: +251115573717
+            </p>
+            <p className="text-base font-medium text-gray-600 ">
+              E-mail: lifecenteret@gmail.com
+            </p>
+            <p className="text-base font-medium text-gray-600 ">
+              P.O.Box: 101519, Addis Ababa-Ethiopia
+            </p>
+            <p className="text-base font-medium text-gray-600 ">
+              Mobile: +251978131614 - Near Temenja Yazh School - Addis Ababa
+            </p>
+          </div>
+          <div className="mt-6 flex justify-start items-start gap-10">
+            <Link to="">
+              <FaFacebook size={30} className="hover:text-[#0075FB]" />
+            </Link>
+            <Link to="">
+              <FaLinkedin size={30} className="hover:text-[#0A66C2]" />
+            </Link>
+            <Link to="">
+              <FaTelegram size={30} className="hover:text-[#29ABEE]" />
+            </Link>
+            <Link to="">
+              <FaYoutube size={30} className="hover:text-[#FF0F1C]" />
+            </Link>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
