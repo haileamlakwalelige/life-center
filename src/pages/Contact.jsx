87f7e72f7caeff495bf5665c96ba08281b5146c6
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-02 07:12:57 +0300

    finalize contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ContactIntro from "../components/contact/ContactIntro";
+import ContactMap from "../components/contact/ContactMap";
+import ContactGetInTouch from "../components/contact/ContactGetInTouch";
 
 const Contact = () => {
   return (
     <div>
       <ContactIntro />
+      <ContactMap />
+      <ContactGetInTouch />
     </div>
   );
 };
