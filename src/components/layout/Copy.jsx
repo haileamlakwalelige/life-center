a03f18f9910a33a0715dbfc849466dcfa6ffcac3
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-03 00:05:23 +0300

    update copyright section

--- src/components/layout/Copy.jsx
+++ src/components/layout/Copy.jsx
@@ -5,10 +5,10 @@ import { FaRegCopyright } from 'react-icons/fa6';
 const Copy = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="bg-[#212741]">
-      <div className="flex justify-center items-center gap-2 text-white py-8">
+    <div className="bg-white ">
+      <div className="flex justify-center items-center gap-2 text-[#212741] py-8 border-t-[1px] border-[#212741] mx-2 sm:mx-6 md:mx-12 lg:mx-20">
         <FaRegCopyright size={30} />
-        <p>{currentYear} Habesha Digital Marketing. All right reserved.</p>
+        <p>{currentYear} Axis Marketing Service. All right reserved.</p>
       </div>
     </div>
   );
