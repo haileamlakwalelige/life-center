8da44dbdfbd966365038881a6224a6e46ac60a3b
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-03 23:32:52 +0300

    update about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Team from "../components/about/Team";
+import AboutIntro from "../components/about/AboutIntro";
+import CallToAction from "../components/Reusable/CallToAction";
+import AboutUs from "../components/about/AboutUs";
+import Value from "../components/about/Value";
+import Mission from "../components/about/Mission";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <AboutIntro />
+      <AboutUs />
+      <Mission />
+      <Team />
+      <Value />
+      <CallToAction />
+    </div>
+  );
+};
 
-export default About
+export default About;
