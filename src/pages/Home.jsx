b8518d2f6944f3e5c986f1b6f76722a9dd5ea2bf
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-04 00:44:29 +0300

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import HomeIntro from "../components/home/HomeIntro";
+import AboutUs from "../components/about/AboutUs";
+import WhyUs from "../components/home/WhyUs";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <HomeIntro />
+      <AboutUs />
+      <WhyUs />
+    </div>
+  );
+};
 
 export default Home
