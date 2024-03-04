6d65f410312f37f1c1feeb83f603b14a17f001a9
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-04 10:00:09 +0300

    add elements to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import HomeIntro from "../components/home/HomeIntro";
 import AboutUs from "../components/about/AboutUs";
 import WhyUs from "../components/home/WhyUs";
+import CallToAction from "../components/Reusable/CallToAction";
+import BlogBlog from "../components/blog/BlogBlog";
+import HomeProgram from "../components/home/HomeProgram";
+import HomeGallery from "../components/home/HomeGallery";
 
 const Home = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <HomeIntro />
       <AboutUs />
       <WhyUs />
+      <HomeProgram />
+      <BlogBlog />
+      <HomeGallery />
+      <CallToAction />
     </div>
   );
 };
