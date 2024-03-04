de1ee2d518f77bf098b545e62a30567e76708a61
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-04 14:45:33 +0300

    update program

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import CallToAction from "../components/Reusable/CallToAction";
+import HomeGallery from "../components/home/HomeGallery";
+import ProgramValue from "../components/program/ProgramValue";
+import WhyUs from "../components/home/WhyUs";
 
 const Program = () => {
   return (
-    <div>Program</div>
-  )
-}
+    <div>
+      <WhyUs />
+      <ProgramValue />
+      <HomeGallery />
+      <CallToAction />
+    </div>
+  );
+};
 
 export default Program
