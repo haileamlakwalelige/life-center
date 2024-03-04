b12b38c6ddad96ab169e651b50938efbf2e01ecf
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-04 10:26:21 +0300

    call testimonial

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,8 @@ import CallToAction from "../components/Reusable/CallToAction";
 import AboutUs from "../components/about/AboutUs";
 import Value from "../components/about/Value";
 import Mission from "../components/about/Mission";
+import Testimonials from '../components/Reusable/Testimonials';
+import reviews from '../data/data';
 
 const About = () => {
   return (
@@ -14,6 +16,8 @@ const About = () => {
       <Mission />
       <Team />
       <Value />
+      {/* <Testimonials /> */}
+      <Testimonials reviews={reviews}/>
       <CallToAction />
     </div>
   );
