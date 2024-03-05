e1e1775c0faf9eb3235e05e56058d174d7485f2a
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-05 10:27:13 +0300

    update testimonial section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ import CallToAction from "../components/Reusable/CallToAction";
 import AboutUs from "../components/about/AboutUs";
 import Value from "../components/about/Value";
 import Mission from "../components/about/Mission";
-import Testimonials from '../components/Reusable/Testimonials';
-import reviews from '../data/data';
+// import Testimonials from '../components/Reusable/Testimonials';
+// import reviews from '../data/data';
 
 const About = () => {
   return (
@@ -17,7 +17,7 @@ const About = () => {
       <Team />
       <Value />
       {/* <Testimonials /> */}
-      <Testimonials reviews={reviews}/>
+      {/* <Testimonials reviews={reviews}/> */}
       <CallToAction />
     </div>
   );
