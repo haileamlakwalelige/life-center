a44acaee84aff42215edbca6d90b2ec965aa5f0c
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-09 11:37:19 +0300

    update blog detail

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -10,7 +10,7 @@ const BlogDetail = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `http://192.168.100.29:8000/api/blogs/${id}`
+          `https://admin.lifecenterethiopia.com/api/blogs/${id}`
         );
         const data = await response.json();
         // console.log(data);
@@ -24,17 +24,21 @@ const BlogDetail = () => {
 
   // Now, you can safely access properties of the single object without encountering errors
   return (
-    <div className="mt-32 flex flex-col justify-center items-center px-2 sm:px-6 md:px-12 lg:px-20">
-      <img
-        src={single.image} // Example of accessing properties
-        alt=""
-        className="max-h-[80vh]  w-[80vw]"
-      />
-      <p className="font-bold text-[20px] sm:text-[25px] md:text-[30px] lg:text-[35px] py-5 mont text-slate-800">
-        {single.title}
-      </p>
-      <p className="text-[16px] md:text-[20px] pb-5">{single.title}</p>
-      <p className="text-[16px] text-end">{single.author}</p>
+    <div className="mt-32 py-10 flex flex-col justify-center items-center px-2 sm:px-6 md:px-12 lg:px-20">
+      {single && (
+        <>
+          <img
+            src={` https://admin.lifecenterethiopia.com/${single.image}`} // Example of accessing properties
+            alt=""
+            className="max-h-[60vh]  w-[80vw] mt-6 rounded-xl"
+          />
+          <p className="font-bold text-[20px] sm:text-[25px] md:text-[30px] lg:text-[35px] py-5 mont text-slate-800">
+            {single.title}
+          </p>
+          <p className="text-[16px] md:text-[20px] pb-5">{single.title}</p>
+          <p className="text-[16px] text-end">{single.author}</p>
+        </>
+      )}
     </div>
   );
 };
