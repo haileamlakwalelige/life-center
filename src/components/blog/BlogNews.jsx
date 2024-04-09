992f61511e71bd898553cfe302490f617c914e50
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-09 11:37:08 +0300

    update the blog news section

--- src/components/blog/BlogNews.jsx
+++ src/components/blog/BlogNews.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import bl from "../../assets/blog1.jpg";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -10,7 +9,9 @@ const BlogNews = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://192.168.100.29:8000/api/news");
+        const response = await fetch(
+          "https://admin.lifecenterethiopia.com/api/news"
+        );
         const data = await response.json();
         // console.log(data); // assuming data is JSON
         SetNews(data); // update state with fetched data
@@ -27,36 +28,36 @@ const BlogNews = () => {
           News - Life Center Transforms Ethiopia.
         </p>
         <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-col">
-            {news.map((item, index) => (
+          <div className="flex flex-col justify-center items-center">
+            {news.map((item) => (
               <div
                 key={item.id}
-                className="flex flex-wrap  lg:flex-nowrap my-10 items-start gap-10 justify-center border-[1px] rounded-xl "
+                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10  border-primary border-[1px]  rounded-xl p-3 min-w-full gap-10"
               >
-                <img
-                  src={item.thumbnail}
-                  alt=""
-                  className="lg:h-[400px] lg:w-[400px] rounded-l-xl"
-                />
-                <div className="flex flex-col items-center px-2 sm:px-4">
-                  <div>
-                    <p className="text-primary text-2xl font-medium py-4">
-                      {item.title}
-                    </p>
-                    <p className="text-slate-800 justify-between ">
-                      {item.description}
-                    </p>
-                    <div className="flex flex-col justify-end text-end items-end text-slate-800 py-4">
-                      <p>{item.date}</p>
-                      <div className="flex gap-2 justify-end text-end items-end my-10">
-                        <Link to={`/news/${item.id}`}>
-                          <div className="lg:min-w-[200px] bg-primary hover:text-primary hover:bg-[#F6FDFE] hover:border-[1px] hover:border-gray-100 py-2 px-10 text-white text-xl rounded-xl justify-end items-end flex gap-2">
-                            <button>Read More</button>
-                            <FaArrowRight size={24} className="mt-1.5" />
-                          </div>
-                        </Link>
+                <div className="col-span-1">
+                  <img
+                    src={` https://admin.lifecenterethiopia.com/${item.thumbnail}`}
+                    alt=""
+                    className="min-h-full min-w-full rounded-xl"
+                  />
+                </div>
+                <div className="col-span-1 lg:col-span-2 flex flex-col justify-start">
+                  <p className="text-primary text-2xl font-medium py-4">
+                    {item.title}
+                  </p>
+                  <p className="text-slate-800 line-clamp-4">
+                    {item.description}
+                  </p>
+                  <p className="text-end justify-end items-end flex-end pt-2">
+                    {item.date}
+                  </p>
+                  <div className="flex gap-2 justify-end items-end my-10">
+                    <Link to={`/news/${item.id}`}>
+                      <div className="lg:min-w-[200px] bg-primary hover:text-primary hover:bg-[#F6FDFE] hover:border-[1px] hover:border-gray-100 py-2 px-10 text-white text-xl rounded-xl flex gap-2">
+                        <button>Read More</button>
+                        <FaArrowRight size={24} className="mt-1.5" />
                       </div>
-                    </div>
+                    </Link>
                   </div>
                 </div>
               </div>
