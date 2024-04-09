824d65d587215a945b23402139c5ee35aeed34ca
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-04-09 11:37:30 +0300

    update news detail

--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -10,7 +10,7 @@ const NewsDetail = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "http://192.168.100.29:8000/api/news/" + id
+          "https://admin.lifecenterethiopia.com/api/news/" + id
         );
         const data = await response.json();
         console.log(data); // assuming data is JSON
@@ -21,13 +21,23 @@ const NewsDetail = () => {
     };
     fetchData();
   }, []);
+
   return (
-    <div className="mt-32 flex flex-col justify-center items-center mont px-2 sm:px-6 md:px-12 lg:px-20">
-      <img src={blog1} alt="" className="max-h-[80vh]  w-[80vw]" />
-      <p className="font-bold text-[20px] sm:text-[25px] md:text-[30px] lg:text-[35px] py-5 mont text-slate-800">
-        {single.title}
-      </p>
-      <p className=" text-[16px] md:text-[20px]">{single.content}</p>
+    <div className="mt-32 py-10 flex flex-col justify-center items-center mont px-2 sm:px-6 md:px-12 lg:px-20">
+      {single && (
+        <>
+          <img
+            src={` https://admin.lifecenterethiopia.com/${single.thumbnail}`}
+            alt=""
+            className="max-h-[60vh] w-[80vw] rounded-xl mt-6"
+          />
+          <p className="font-bold text-[20px] sm:text-[25px] md:text-[30px] lg:text-[35px] py-5 mont text-slate-800">
+            {single.title}
+          </p>
+          {/* <p className="text-[16px] md:text-[20px]">{single.description}</p> */}
+          <div className="text-[16px] md:text-[20px]" dangerouslySetInnerHTML={{ __html: single.description }} />
+        </>
+      )}
     </div>
   );
 };
